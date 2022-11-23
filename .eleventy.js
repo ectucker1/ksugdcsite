2630575c84ab3cdcaa1af52ddaa4a2badfce2fe6
Ethan Tucker <ectucker1@gmail.com>
2022-11-22 21:42:37 -0600

    Initial 11ty scaffolding

---
+++ .eleventy.js
@@ -0,0 +1,67 @@
+const { EleventyRenderPlugin } = require("@11ty/eleventy");
+const Image = require("@11ty/eleventy-img");
+
+// Site directories
+const dir = {
+    input: 'src',
+    output: '_site',
+    includes: '_includes',
+    layouts: '_layouts',
+    data: '_data',
+};
+
+// Template language for the site: https://www.11ty.dev/docs/languages/liquid/
+const TEMPLATE_ENGINE = 'liquid';
+
+module.exports = (eleventyConfig) => {
+    // Watch targets
+    eleventyConfig.addWatchTarget(`${dir.input}/assets/styles`);
+
+    // Static assets
+    eleventyConfig.addPassthroughCopy('src/assets/images');
+    eleventyConfig.addPassthroughCopy('src/assets/fonts');
+
+    // Render plugin
+    eleventyConfig.addPlugin(EleventyRenderPlugin, {
+        tagName: 'renderTemplate',
+        tagNameFile: 'renderFile',
+    });
+
+    // Image shortcode
+    // Inspired by https://www.aleksandrhovhannisyan.com/blog/eleventy-image-plugin/
+    const imageShortcode = async (
+        src,
+        alt = 'Header',
+        widths = [400, 800, 1280, 2560],
+        formats = ['webp', 'jpeg'],
+        sizes = '100vw'
+    ) => {
+        const imageMetadata = await Image('src/assets/images/' + src, {
+            widths: [...widths, null],
+            formats: [...formats, null],
+            outputDir: './_site/assets/images',
+            urlPath: '/assets/images',
+        });
+
+        const imageAttributes = {
+            alt,
+            sizes,
+            loading: "lazy",
+            decoding: "async",
+        };
+
+        return Image.generateHTML(imageMetadata, imageAttributes);
+    };
+    eleventyConfig.addShortcode("image", imageShortcode)
+
+    // robots.txt
+    eleventyConfig.addPassthroughCopy('src/robots.txt');
+
+    return {
+        dir,
+        dataTemplateEngine: TEMPLATE_ENGINE,
+        markdownTemplateEngine: TEMPLATE_ENGINE,
+        htmlTemplateEngine: TEMPLATE_ENGINE,
+        templateFormats: ['html', 'md', TEMPLATE_ENGINE],
+    };
+};
---
+++ README.md
@@ -0,0 +1,9 @@
+# K-State Game Development Club Website
+
+This is a development version of the K-State Game Development Website.
+
+It is implemented using the [11ty](https://www.11ty.dev/) static site generator.
+
+The design is based on the [old version](https://github.com/daytonltaylor/gdc.cs.ksu.edu) by Dayton Taylor.
+However, all the interactive components have been removed to simplify maintenance.
+It is a static site with no database requirements.
